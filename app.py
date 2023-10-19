947b209e7706a00ada76097029e2fc6e6c3e8c2d
Jin2oo2 <nojinhiroaki@icloud.com>
2023-10-19 13:52:39 +0100

    register, login, homegae done

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Flask App</h1>'
+    registerURL = url_for('register')
+    loginURL = url_for('login')
+    return render_template('home.html', registerURL=registerURL, loginURL=loginURL)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return '<h1>Log in page</h1>'
-    
+        return render_template('login.html')
