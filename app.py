60246791836cd2c7506cc0aeb067949e301c7407
InertiaDrift <owaisk098@gmail.com>
2023-10-23 21:23:09 +0100

    Added Feedback, parent layout and css style

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
+import sqlite3 as sql
+from models import create_feedback_table
+
+
+db_path = 'C:\\Users\\owais\\Desktop\\Yggdrasill\\KS\\komodo-hub\\ABC.db'
+
+create_feedback_table()
 
 app = Flask(__name__)
 
@@ -17,3 +24,24 @@ def register():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+
+
+@app.route('/feedback' , methods = ['GET','POST'], endpoint='feedback')
+def feedback():
+    if request.method == 'GET':
+        return render_template('feedback.html')
+ 
+    if request.method == 'POST':
+        suggestion = request.form['suggestion']
+        
+        
+        conn = sql.connect('ABC.db')
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO feedback (submission_date, suggestion) VALUES (CURRENT_TIMESTAMP, ?)", (suggestion,))
+
+        conn.commit()
+        conn.close()
+        
+        return redirect(url_for('feedback'))
+    return render_template('feedback.html')  
