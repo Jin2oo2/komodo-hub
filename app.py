f830a081481c430a51a955e45751e81b4b6caf03
Jin2oo2 <nojinhiroaki@icloud.com>
2023-10-28 15:15:56 +0100

    Initialised database

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask, request, render_template, url_for, redirect, abort
 import sqlite3 as sql
 from models import create_feedback_table
-# from flask_sqlalchemy import SQLAlchemy
-# #from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager
+import os
 
-
-db_path = 'C:\\Users\\owais\\Desktop\\Yggdrasill\\KS\\komodo-hub\\ABC.db'
+db_path = 'C:\\Users\\owapip install big-ois\\Desktop\\Yggdrasill\\KS\\komodo-hub\\ABC.db'
 create_feedback_table()
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///komodoDB.db'
-# db.init_app(app)
+app.secret_key = os.urandom(24)
+db.init_app(app)
 
-#login_manager = LoginManager()
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 @app.route('/')
 def hello():
@@ -63,8 +68,8 @@ def feedback():
         return redirect(url_for('feedback'))
     return render_template('feedback.html')  
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String, unique=True, nullable=False)
-#     password = db.Column(db.String, nullable=False)
-#     userType = db.Column(db.String, nullable=False)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    userType = db.Column(db.String, nullable=False)
