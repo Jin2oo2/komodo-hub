2a68936cdfe16d2e2c32da96b55f32b8b56ae8ab
Jin2oo2 <nojinhiroaki@icloud.com>
2023-10-28 12:08:33 +0100

    added radio button

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, render_template, url_for, redirect, abort
 import sqlite3 as sql
 from models import create_feedback_table
+# from flask_sqlalchemy import SQLAlchemy
+# #from flask_login import LoginManager
 
 
 db_path = 'C:\\Users\\owais\\Desktop\\Yggdrasill\\KS\\komodo-hub\\ABC.db'
 create_feedback_table()
 
+# db = SQLAlchemy()
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///komodoDB.db'
+# db.init_app(app)
+
+#login_manager = LoginManager()
+
 
 @app.route('/')
 def hello():
@@ -52,3 +62,9 @@ def feedback():
         
         return redirect(url_for('feedback'))
     return render_template('feedback.html')  
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String, unique=True, nullable=False)
+#     password = db.Column(db.String, nullable=False)
+#     userType = db.Column(db.String, nullable=False)
