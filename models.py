9ffeac1b5b99cfee021977245c79be5b112bec18
InertiaDrift <owaisk098@gmail.com>
2023-11-13 01:28:07 +0000

    Added Entire Species Section

--- models.py
+++ models.py
@@ -17,3 +17,60 @@ def create_feedback_table():
 
     conn.commit()
     conn.close()
+
+# Connect to the database
+conn = sqlite3.connect('DBTest1.db')
+cursor = conn.cursor()
+
+# Create the Species table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS SPECIES (
+        Species_ID INTEGER PRIMARY KEY,
+        Species_Name TEXT,
+        Species_Area TEXT,
+        Species_Status TEXT,
+        Species_Population TEXT
+    )
+''')
+
+# Create the Students table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS STUDENTS (
+        Student_ID INTEGER PRIMARY KEY,
+        First_Name TEXT,
+        Last_Name TEXT,
+        Email TEXT
+    )
+''')
+
+# Create the Schools table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS SCHOOLS (
+        School_ID INTEGER PRIMARY KEY,
+        School_Name TEXT,
+        Supervisor_Name TEXT,
+        Supervisor_Phone NUMERIC,
+        Access_Code TEXT
+    )
+''')
+
+# Create the Sightings table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS SIGHTINGS (
+        Sighting_ID INTEGER PRIMARY KEY,
+        Species_ID INTEGER,
+        Student_ID INTEGER,
+        School_ID INTEGER,
+        Species_Name TEXT
+        Location TEXT,
+        Date TEXT,
+        FOREIGN KEY (Species_ID) REFERENCES SPECIES (Species_ID),
+        FOREIGN KEY (Student_ID) REFERENCES STUDENTS (Student_ID),
+        FOREIGN KEY (School_ID) REFERENCES SCHOOLS (School_ID)
+    )
+''')
+
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
